be8545523f58c168e5409a4a14dcaa12f034500f
dzaba1 <blazej.zabialowicz@gmail.com>
2026-02-15 13:56:21 +0100

    First unit test

---
+++ src/Dzaba.HomeSecurity.LogsIngestion/src/Dzaba.HomeSecurity.LogsIngestion.Tests/LogsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using Dzaba.HomeSecurity.LogsIngestion.Contracts;
+using Dzaba.HomeSecurity.LogsIngestion.Controllers;
+using Dzaba.TestUtils;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace Dzaba.HomeSecurity.LogsIngestion.Tests;
+
+[TestFixture]
+public class LogsControllerTests : ControllerUnitTestFixture
+{
+    private LogsController CreateSut()
+    {
+        return Fixture.Create<LogsController>();
+    }
+
+    [Test]
+    public async Task Ingest_WhenLogs_ThenThoseArePropagated()
+    {
+        var eventPublisher = Fixture.FreezeMock<IEventPublisher>();
+        var requestBody = Fixture.Create<IngestLogsRequest>();
+
+        var sut = CreateSut();
+
+        var result = await sut.Ingest(requestBody).ConfigureAwait(false);
+        result.Should().BeOfType<AcceptedResult>();
+
+        foreach (var evt in requestBody.Events)
+        {
+            eventPublisher.Verify(p => p.PublishAsync(evt), Times.Once());
+        }
+    }
+}
