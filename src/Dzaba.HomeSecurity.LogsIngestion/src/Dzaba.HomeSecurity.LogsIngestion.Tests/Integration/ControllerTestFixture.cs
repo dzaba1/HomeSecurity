9e028467815885fce044f028cc159cc016b006ca
dzaba1 <blazej.zabialowicz@gmail.com>
2026-02-17 17:39:43 +0100

    Setting up integration tests

---
+++ src/Dzaba.HomeSecurity.LogsIngestion/src/Dzaba.HomeSecurity.LogsIngestion.Tests/Integration/ControllerTestFixture.cs
@@ -0,0 +1,56 @@
+﻿using Dzaba.IntegrationTestUtils;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace Dzaba.HomeSecurity.LogsIngestion.Tests.Integration;
+
+public abstract class ControllerTestFixture
+{
+    private WebApplicationFactory<Program> factory;
+    private Action<IConfigurationBuilder> configBuilderCallback = null;
+
+    [SetUp]
+    public void SetupBuilder()
+    {
+        factory = new WebApplicationFactory<Program>().WithWebHostBuilder(b =>
+        {
+            b.ConfigureAppConfiguration((context, builder) =>
+            {
+                if (configBuilderCallback != null)
+                {
+                    configBuilderCallback(builder);
+                }
+            })
+            .ConfigureTestServices(services =>
+            {
+                services.AddSerilogConsoleLogging();
+            });
+        });
+    }
+
+    [TearDown]
+    public void CleanupFactory()
+    {
+        factory?.Dispose();
+    }
+
+    protected HttpClient CreateClient(Action<IConfigurationBuilder> configBuilderCallback = null)
+    {
+        this.configBuilderCallback = configBuilderCallback;
+        return factory.CreateClient();
+    }
+
+    protected async Task<string> AssertAsync(HttpResponseMessage resp)
+    {
+        resp.Should().NotBeNull();
+        var respStr = await resp.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        this.Invoking(_ => resp.EnsureSuccessStatusCode())
+            .Should().NotThrow(respStr);
+
+        return respStr;
+    }
+}
---
+++ src/Dzaba.HomeSecurity.LogsIngestion/src/Dzaba.HomeSecurity.LogsIngestion.Tests/Integration/LogsControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Dzaba.HomeSecurity.LogsIngestion.Contracts;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Dzaba.HomeSecurity.LogsIngestion.Tests.Integration;
+
+[TestFixture]
+public class LogsControllerTests : ControllerTestFixture
+{
+    [Test]
+    public async Task AAA()
+    {
+        var client = CreateClient();
+
+        var request = new IngestLogsRequest
+        {
+            Events = [
+                new Events
+                    {
+                        EventId = Guid.NewGuid(),
+                        HomeId = "1",
+                        DeviceId = "2",
+                        Level = EventsLevel.Info,
+                        Message = "Test message"
+                    }
+                ]
+        };
+
+        var resp = await client.PostAsJsonAsync("/api/v1/logs", request);
+        resp.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
