ad6b4750617e6ab60de8b5ddb6bdad3529bc8157
dzaba1 <blazej.zabialowicz@gmail.com>
2026-02-20 19:12:19 +0100

    swagger ui

--- src/Dzaba.HomeSecurity.LogsIngestion/src/Dzaba.HomeSecurity.LogsIngestion/Program.cs
+++ src/Dzaba.HomeSecurity.LogsIngestion/src/Dzaba.HomeSecurity.LogsIngestion/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddLogsIngestionLogging();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -15,6 +17,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
