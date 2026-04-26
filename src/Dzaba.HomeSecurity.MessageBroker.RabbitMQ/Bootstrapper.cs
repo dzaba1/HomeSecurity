e227e0a5aa4ea41837fce1695d2b1e040a720ee8
dzaba1 <blazej.zabialowicz@gmail.com>
2026-04-26 15:29:24 +0200

    rabbitmq lib

---
+++ src/Dzaba.HomeSecurity.MessageBroker.RabbitMQ/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Dzaba.HomeSecurity.LogsIngestion;
+
+internal static class Bootstrapper
+{
+    public static IServiceCollection AddRabbitMQMessageBroker(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        return services;
+    }
+}
---
+++ src/Dzaba.HomeSecurity.MessageBroker.RabbitMQ/RabbitMQEventPublisher.cs
@@ -0,0 +1,24 @@
+﻿using Dzaba.HomeSecurity.LogsIngestion.Contracts;
+using Dzaba.HomeSecurity.MessageBroker.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace Dzaba.HomeSecurity.MessageBroker.RabbitMQ;
+
+internal sealed class RabbitMQEventPublisher : IEventPublisher
+{
+    private readonly ILogger<RabbitMQEventPublisher> logger;
+
+    public RabbitMQEventPublisher(ILogger<RabbitMQEventPublisher> logger)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+
+        this.logger = logger;
+    }
+
+    public Task PublishAsync(Events evt)
+    {
+        ArgumentNullException.ThrowIfNull(evt);
+
+        throw new NotImplementedException();
+    }
+}
